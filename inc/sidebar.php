d050a55116b176e9ab044f5422b747e1ba7900d5
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-06-06 13:37:54 +0100

    Added the profile page in admin space

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -5,7 +5,7 @@ echo '  <div class="l-navbar" id="nav-bar">
   <div> <a href="#" class="nav_logo"><span
         class="nav_logo-name">Dashboard Admin</span> </a>
     <div class="nav_list">
-         <a href="Dashboard.php" class="nav_link active"> <i class=\'bx bx-grid-alt nav_icon\'></i> <span class="nav_name">Dashboard</span> </a> 
+         <a href="dashboard.php" class="nav_link active"> <i class=\'bx bx-grid-alt nav_icon\'></i> <span class="nav_name">Dashboard</span> </a> 
          <a href="usersDash.php" class="nav_link"> <i class=\'bx bx-user nav_icon\'></i> <span class="nav_name">Users</span> </a>
          <a href="trainersDash.php" class="nav_link"><i class=\'bx bxs-medal\' ></i> <span class="nav_name">Trainers</span></a> 
          <a href="adminDash.php" class="nav_link"><i class=\'bx bx-wrench\'></i><span class="nav_name">Admin</span> </a> 
